daa2db3340734df6b72755bf319e75dc1dd5b43d
karaagac <ikaraagac@gmail.com>
2020-02-22 11:37:30 -0500

    Create kur.sh

---
+++ kur.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get install vim htop xterm xbindkeys newsboat w3m ranger feh mpv rofi vlc mplayer rtv moc curl git vifm xclip zathura xbacklight mupdf moreutils renameutils npm mpg123 pgpgpg translate-shell ffmpeg task-spooler urlview python-pip chromium-browser sdcv pandoc hwinfo w3m-img tmux xdotool libmodule-build-perl libtinfo-dev libncurses5-dev libreadline-dev libunicode-linebreak-perl frei0r-plugins npm surfraw surfraw-extra
+
+
+echo "instaling youtube-dl"
+sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+echo "installing i3wm"
+sudo apt-get install i3 suckless-tools i3status
